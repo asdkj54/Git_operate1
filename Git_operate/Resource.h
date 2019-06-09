0b8ab62c751708fffe82edd6416f1019d24234ba
asdkj54 <1450438214@qq.com>
2019-06-09 20:48:23 +0800

    dfzf

--- Git_operate/Resource.h
+++ Git_operate/Resource.h
@@ -7,15 +7,16 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_GIT_OPERATE_DIALOG          102
 #define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     130
 #define IDC_BUTTON1                     1000
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
