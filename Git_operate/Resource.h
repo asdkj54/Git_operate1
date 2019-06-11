732fc111c49adc8d0bb62a4f72a984df05f0504f
asdkj54 <1450438214@qq.com>
2019-06-11 11:49:25 +0800

    j

--- Git_operate/Resource.h
+++ Git_operate/Resource.h
@@ -9,6 +9,7 @@
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     130
 #define IDC_BUTTON1                     1000
+#define IDC_TREE1                       1002
 
 // Next default values for new objects
 // 
@@ -16,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
